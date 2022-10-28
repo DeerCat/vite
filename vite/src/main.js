61730018a0586d6bda6b0de5c3edf36da4a5060e
Deercat <zyanlumia@hotmail.com>
2022-10-28 10:49:38 +0800

    呵呵

--- vite/src/main.js
+++ vite/src/main.js
@@ -2,9 +2,9 @@ import {createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import './assets/main.css'
-
+import request from './utils/request.js'
 const app = createApp(App)
 
 app.use(router)
-
+app.config.globalProperties.$http = request
 app.mount('#app')
--- vite/src/views/Demo.vue
+++ vite/src/views/Demo.vue
@@ -1,13 +1,29 @@
 <template>
     <div>
-        <h1>demo</h1>    <DemoTest />
+        <h1>demo</h1>       
+        <DemoTest />
     </div>
-    
 </template>
-<script setup>
-    import request from '../utils/request.js'
+<script>
+    import {onMounted} from 'vue'
     import DemoTest from '../components/Reactive&Ref.vue'
-    request({
-        url:'/api/V1/Bookuu/GetHomePageBanner?bannerType=5&cardNo=933000000000024936&channel_id=100001'
-    }).then(res=>{})
+
+    onMounted(()=>{
+        console.log(this)
+        // $http({
+        //     url:'/api/V1/Bookuu/GetHomePageBanner?bannerType=5&cardNo=933000000000024936&channel_id=100001'
+        // }).then(res=>{})
+    })
+    export default{
+        setup(props,ctx){
+            console.log(props,ctx)
+        },
+        mounted(){
+            console.log(this)
+             this.$http({
+                    url:'/api/V1/Bookuu/GetHomePageBanner?bannerType=5&cardNo=933000000000024936&channel_id=100001'
+                }).then(res=>{})
+        },
+    }
+   
 </script>
